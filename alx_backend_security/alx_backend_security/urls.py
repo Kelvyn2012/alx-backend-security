41f91392094ee0734dda517e7421b429a01082f8
kelvyn2012 <kudufo@gmail.com>
2026-01-06 16:25:14 +0100

    added docker to maintain my code in all machines

--- alx_backend_security/alx_backend_security/urls.py
+++ alx_backend_security/alx_backend_security/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"message": "Welcome to ALX Backend Security"})
+
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("ip_tracking/", include("ip_tracking.urls")),
 ]
